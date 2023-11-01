d9166ddb4016f7cbe3fd1603ec8fed3e0f9fbb8e
Ximelele <druzbacky.martin@gmail.com>
2023-11-01 16:34:35 +0100

    Move clawler to different file and made it class for easier usage from multiple website

---
+++ Crawler.py
@@ -0,0 +1,82 @@
+import os
+import requests
+import re
+from urllib.parse import urljoin
+
+class WebCrawler:
+
+    def __init__(self,starting_url, allowed_domain, user_agent, prefix_domain, regex, robots):
+        self.starting_url = starting_url
+        self.allowed_domain = allowed_domain
+        self.user_agent = user_agent
+        self.prefix_domain = prefix_domain
+        self.regex = regex
+        self.robots = robots
+
+    def can_crawl(self, url, user_agent, robots):
+        try:
+            robots_url = urljoin(url, robots)
+            robots_response = requests.get(robots_url)
+            if robots_response.status_code == 200:
+                robots_content = robots_response.text
+                for line in robots_content.split('\n'):
+                    if line.startswith("User-agent:") and user_agent in line:
+                        allow_access = True
+                        for line in robots_content.split('\n'):
+                            if line.startswith("Disallow:") and re.match(r"Disallow:\s*/", line):
+                                allow_access = False
+                        return allow_access
+                return True
+            else:
+                return True
+        except Exception as e:
+            print(f"Error checking robots.txt: {str(e)}")
+            return True
+
+    def web_crawler(self, new_url = None, allowed_domain = None, user_agent = None, prefix_domain = None, regex = None, robots = None):
+        if new_url is None:
+            new_url = self.starting_url
+            allowed_domain = self.allowed_domain
+            user_agent = self.user_agent
+            prefix_domain = self.prefix_domain
+            regex = self.regex
+            robots = self.robots
+        visited_urls = set()  # set aby neboli duplikaty
+        to_crawl = [new_url]
+        # prefix_domain = "https://www.dotabuff.com"
+        while to_crawl:
+            url: str = to_crawl.pop()
+            if not self.can_crawl(url, user_agent, robots):
+                continue
+            # print('Idzem crawlovat kkt')
+            # return
+            try:
+                response = requests.get(url, headers=user_agent)
+                if response.status_code == 200:
+                    print(f'Crawling url: {url}')
+                    links = re.findall(regex, response.text)
+
+                    # # pozrie sa ci sa nachadza zakladna domena v vyextrahovanom linku
+                    modified_links = [prefix_domain + link if not re.search(allowed_domain, link) else link for link in
+                                      links]
+                    # modified_links = soup.find_all('a')
+                    for link in modified_links:
+                        # print(link.get('href'))
+                        if link not in visited_urls and re.search(allowed_domain, link):
+                            # append_to_file(link)
+                            visited_urls.add(link)
+                            if not re.search(r'wiki/(Module|Special|File|Template|User)', link):
+                                to_crawl.append(link)
+
+                    if "buff" in prefix_domain:
+                        file_name = url.replace("https://www.dotabuff.com/", "").replace('/', '_') + '.txt'
+                        file_path = os.path.join("dotabuff/", file_name)
+                        with open(file_path, "w") as file:
+                            file.write(response.text)
+                    else:
+                        file_name = url.replace("https://dota2.fandom.com/", "").replace('/', '_') + '.txt'
+                        file_path = os.path.join("dotafandom/", file_name)
+                        with open(file_path, "w") as file:
+                            file.write(response.text)
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
