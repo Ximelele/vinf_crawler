d75e4352fcafb68ef13e8ad8ae3cd611efd36ca5
Ximelele <druzbacky.martin@gmail.com>
2023-12-06 15:01:01 +0000

    Added unittest

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import Crawler
 # import sparkMerge
 import lucene
 
+
 from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType, TextField,StringField
@@ -12,7 +13,62 @@ from org.apache.lucene.store import MMapDirectory,NIOFSDirectory
 from org.apache.lucene.search import IndexSearcher
 import os
 
+import unittest
 
+# def test_upper(self):
+#         self.assertEqual('foo'.upper(), 'FOO')
+
+#     def test_isupper(self):
+#         self.assertTrue('FOO'.isupper())
+#         self.assertFalse('Foo'.isupper())
+
+#     def test_split(self):
+#         s = 'hello world'
+#         self.assertEqual(s.split(), ['hello', 'world'])
+#         # check that s.split fails when the separator is not a string
+#         with self.assertRaises(TypeError):
+#             s.split(2)
+import unittest
+
+class TestSearchQuery(unittest.TestCase):
+    
+    def setUp(self):
+        self.current_test_name = self.id().split('.')[-1]
+
+    def test_abbadon_couters_in_docs(self):
+        print(f"Running test: {self.current_test_name}")
+        analyzer = StandardAnalyzer()
+        directory = MMapDirectory(Paths.get(index_path))
+        searcher = IndexSearcher(DirectoryReader.open(directory))
+        searchTerm  = "Abaddon counters"
+        term_query = QueryParser("content",analyzer).parse(searchTerm)
+        max_results = 20
+        top_docs: IndexSearcher = searcher.search(term_query, max_results).scoreDocs
+        doc_id = top_docs[0].doc
+        self.assertEqual(doc_id, 363)
+
+    def test_term_not_in_docs(self):
+        print(f"Running test: {self.current_test_name}")
+        analyzer = StandardAnalyzer()
+        directory = MMapDirectory(Paths.get(index_path))
+        searcher = IndexSearcher(DirectoryReader.open(directory))
+        searchTerm  = "Ferko Mrkvicka na hrade"
+        term_query = QueryParser("content",analyzer).parse(searchTerm)
+        max_results = 20
+        top_docs: IndexSearcher = searcher.search(term_query, max_results).scoreDocs
+        self.assertEqual(len(top_docs), 0)
+
+    def test_right_input_wrong_doc(self):
+        print(f"Running test: {self.current_test_name}")
+        analyzer = StandardAnalyzer()
+        directory = MMapDirectory(Paths.get(index_path))
+        searcher = IndexSearcher(DirectoryReader.open(directory))
+        searchTerm  = "Abaddon counter"
+        term_query = QueryParser("content",analyzer).parse(searchTerm)
+        max_results = 20
+        top_docs: IndexSearcher = searcher.search(term_query, max_results).scoreDocs
+        doc_id = top_docs[0].doc
+        self.assertNotEqual(doc_id, 363)
 
 
 def index_files(root_folder, index_path):
@@ -58,7 +114,11 @@ def fandom_helper() -> set:
     links_for_fandom.add("/wiki/Anti-Mage")
 
     return links_for_fandom
-
+def boolQuery():
+    test_query = "Axe AND Doom AND Brewmaster NOT Chen"
+    
+    test_query = test_query.split('AND')
+    print(test_query)
 
 if __name__ == "__main__":
     starting_urlFandom = 'https://dota2.fandom.com'
@@ -102,36 +162,41 @@ index_path = "/workspaces/VINF_CRAWLER/index/"
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 # index_files(root_folder, index_path)
 
+# boolQuery()
 
-analyzer = StandardAnalyzer()
-directory = MMapDirectory(Paths.get(index_path))
-searcher = IndexSearcher(DirectoryReader.open(directory))
-searchTerm  = str(input('Search phrase: '))
-term_query = QueryParser("content",analyzer).parse(searchTerm)
-max_results = 100
-top_docs: IndexSearcher = searcher.search(term_query, max_results)
+# analyzer = StandardAnalyzer()
+unittest.main()
+# directory = MMapDirectory(Paths.get(index_path))
+# searcher = IndexSearcher(DirectoryReader.open(directory))
+# searchTerm  = str(input('Search phrase: '))
+# term_query = QueryParser("content",analyzer).parse(searchTerm)
+# max_results = 20
+# top_docs: IndexSearcher = searcher.search(term_query, max_results)
 
-# boolean_query = BooleanQuery.Builder()
-# name_query = QueryParser("name", analyzer).parse("Abaddon")
-# boolean_query.add(name_query, BooleanClause.Occur.MUST)
+# # boolean_query = BooleanQuery.Builder()
+# # name_query = QueryParser("name", analyzer).parse("Abaddon")
+# # boolean_query.add(name_query, BooleanClause.Occur.MUST)
 
-# production_years_query = QueryParser("category", analyzer).parse("Axe")
-# boolean_query.add(production_years_query, BooleanClause.Occur.MUST)
+# # multiple_heroes = QueryParser("category", analyzer).parse("Axe")
+# # boolean_query.add(multiple_heroes, BooleanClause.Occur.MUST)
 
-# results = searcher.search(boolean_query.build(), 1000)
-# # # hits = results.scoreDocs 
+# # results = searcher.search(boolean_query.build(), 1000)
+# # # # hits = results.scoreDocs 
 
 
+# print("[%s] = total matching documents." % len(top_docs.scoreDocs))
 
-for score_doc in top_docs.scoreDocs:
-    doc_id = score_doc.doc
-    document = searcher.doc(doc_id)
-    print(f"Document ID: {doc_id}, Path: {document.get('path')}")
+
+# for score_doc in top_docs.scoreDocs:
+#     doc_id = score_doc.doc
+#     document = searcher.doc(doc_id)
+#     print(f"Score: {score_doc.score} Document ID: {doc_id}, Path: {document.get('path')}")
     
-docID  = int(input('Select doc: '))
-document = searcher.doc(docID)
-print(f"{document.get('path')}\n{document.get('content')}")
-searcher.getIndexReader().close()
+# docID  = int(input('Select doc: '))
+# document = searcher.doc(docID)
+# print(f"{document.get('path')}\n{document.get('content')}")
+# searcher.getIndexReader().close()
+
 
 
 
